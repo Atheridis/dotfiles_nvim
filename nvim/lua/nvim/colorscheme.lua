4990a7dee5ee91058650e0217a24a170469d93c1
Georgios Atheridis <atheridis@tutamail.com>
2022-07-07 09:31:46 +0300

    changed colorscheme

--- nvim/lua/nvim/colorscheme.lua
+++ nvim/lua/nvim/colorscheme.lua
@@ -1,10 +1,15 @@
-local status_ok, onedark = pcall(require, "onedark")
-if not status_ok then
-	vim.notify("Error with colorscheme")
-	return
-end
+-- local status_ok, onedark = pcall(require, "onedark")
+-- if not status_ok then
+-- 	vim.notify("Error with colorscheme")
+-- 	return
+-- end
+--
+-- onedark.setup({
+-- 	style = "deep",
+-- })
+-- onedark.load()
 
-onedark.setup({
-	style = "deep",
-})
-onedark.load()
+-- vim.cmd('colorscheme paramount')
+vim.cmd('set termguicolors')
+vim.cmd('set background=light')
+vim.cmd('colorscheme zenbones')
--- nvim/lua/nvim/plugins.lua
+++ nvim/lua/nvim/plugins.lua
@@ -59,6 +59,14 @@ return packer.startup(function(use)
 
 	-- Colorschemes
 	use("navarasu/onedark.nvim")
+    use("owickstrom/vim-colors-paramount")
+    use {
+    "mcchrish/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    requires = "rktjmp/lush.nvim"
+    }
 
 	-- cmp Plugins
 	use("hrsh7th/nvim-cmp")

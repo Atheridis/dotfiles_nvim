2a448369133c281402bc87cde7859f4bd8599bc0
Georgios Atheridis <atheridis@tutamail.com>
2022-09-06 20:18:25 +0300

    updated colourscheme

--- nvim/lua/nvim/colorscheme.lua
+++ nvim/lua/nvim/colorscheme.lua
@@ -1,18 +1,22 @@
--- local status_ok, onedark = pcall(require, "onedark")
--- if not status_ok then
--- 	vim.notify("Error with colorscheme")
--- 	return
--- end
---
--- onedark.setup({
--- 	style = "deep",
--- })
--- onedark.load()
+local status_ok, onedark = pcall(require, "onedark")
+if not status_ok then
+	vim.notify("Error with colorscheme")
+	return
+end
+
+onedark.setup({
+	style = "deep",
+})
+onedark.load()
 
 -- vim.cmd('colorscheme paramount')
 --
--- vim.cmd('set termguicolors')
--- vim.cmd('set background=light')
+vim.cmd('set termguicolors')
+vim.cmd('set background=dark')
 -- vim.cmd('colorscheme zenbones')
 --
-vim.cmd("colorscheme nofrils-acme")
+-- vim.cmd("colorscheme nofrils-acme")
+
+-- vim.cmd("let g:nofrils_heavylinenumbers=1")
+-- vim.cmd("let g:nofrils_strbackgrounds=1")
+-- vim.cmd("let g:nofrils_heavycomments=1")
